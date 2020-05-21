e91f836381d8c296dcc579ebe2a51986d924a5fa
Hemant Negi <65716172+Hemant-Negi@users.noreply.github.com>
2020-05-21 21:16:26 +0530

    Create python4.py

---
+++ day1/python4.py
@@ -0,0 +1,6 @@
+cost_price=float(input("enter the cost price :"))
+selling_price=float(input("enter the selling price :"))
+profit=selling_price-cost_price
+print('profit :',profit)
+new_selling_price=selling_price+(5/100)*selling_price
+print("new selling price:",new_selling_price)
