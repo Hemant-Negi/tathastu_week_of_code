45279094b8400db59cffeabfd93bf429722923eb
Hemant Negi <65716172+Hemant-Negi@users.noreply.github.com>
2020-05-31 13:25:13 +0530

    Create program3.py

---
+++ day4/program3.py
@@ -0,0 +1,7 @@
+  
+dict1 = {"Hemant":46 , "Ayush":95, "Malan":88, "kartik":73}
+List1 = list(dict1.values())
+print(List1)
+List1.sort()
+elements= List1[-2]
+print("The second maximum element in the dictionary is :",elements)
